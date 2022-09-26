f83969c38cedc77d2faed4f120423e6a6d86ba71
KeithRussel <keithmaniacop@gmail.com>
2022-09-26 13:11:46 +0800

    Add css using styled components

--- src/Components/Articles/Article.js
+++ src/Components/Articles/Article.js
@@ -1,14 +1,22 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { theme } from "../../utils/styles";
+
+const Card = styled.div`
+  background-color: ${() => theme.colors.primary};
+  padding: 1rem;
+  box-shadow: 0px 2px 4px black;
+`;
 
 const Article = ({ article }) => {
   return (
-    <div className="card">
+    <Card>
       {/* {Object.values(article).map((value, index) => (
         <p key={index}>{value}</p>
       ))} */}
       <h1>{article.title}</h1>
       <p>{article.content}</p>
-    </div>
+    </Card>
   );
 };
 
