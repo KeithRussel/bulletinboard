b91f85f97296d7ca59ef3701443a55ad2e55790b
KeithRussel <keithmaniacop@gmail.com>
2022-09-26 08:59:30 +0800

    Remove object.values and change to destructure object

--- src/Components/Articles/Article.js
+++ src/Components/Articles/Article.js
@@ -3,9 +3,11 @@ import React from "react";
 const Article = ({ article }) => {
   return (
     <div className="card">
-      {Object.values(article).map((value, index) => (
+      {/* {Object.values(article).map((value, index) => (
         <p key={index}>{value}</p>
-      ))}
+      ))} */}
+      <h1>{article.title}</h1>
+      <p>{article.content}</p>
     </div>
   );
 };
