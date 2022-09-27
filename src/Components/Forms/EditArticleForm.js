3b633a0b866f40ab66039dc148df7fb64189b6d9
KeithRussel <keithmaniacop@gmail.com>
2022-09-28 01:11:50 +0800

    Integrate Edit Features and fix some bugs

---
+++ src/Components/Forms/EditArticleForm.js
@@ -0,0 +1,66 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { theme } from "../../utils/styles";
+
+const Input = styled.input`
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const Button = styled.input`
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: ${() => theme.colors.secondary};
+  color: ${() => theme.colors.white};
+`;
+
+const EditArticleForm = ({
+  title,
+  setTitle,
+  content,
+  setContent,
+  titleOnChangeHandler,
+  contentOnChangeHandler,
+  handleUpdate,
+}) => {
+  return (
+    <form>
+      <Input
+        type="text"
+        placeholder="Input Title"
+        name="title"
+        value={title}
+        onChange={titleOnChangeHandler}
+        required
+      />
+      <TextArea
+        type="text"
+        placeholder="Input Content"
+        name="content"
+        value={content}
+        onChange={contentOnChangeHandler}
+        required
+      />
+      <Button type="submit" value="Update Article" onClick={handleUpdate} />
+    </form>
+  );
+};
+
+export default EditArticleForm;
--- src/Screens/EditArticleScreen.js
+++ src/Screens/EditArticleScreen.js
@@ -1,7 +1,70 @@
-import React from "react";
+import styled from "@emotion/styled";
+import React, { useState, useEffect } from "react";
+import EditArticleForm from "../Components/Forms/EditArticleForm";
+import { useLocalStorage } from "../Components/LocalStorage/localStorage";
+import { useParams } from "react-router-dom";
+
+const EditArticleScreenMain = styled.div`
+  max-width: 50%;
+  margin: auto;
+`;
 
 const EditArticleScreen = () => {
-  return <div>EditArticleScreen</div>;
+  const [articleList, setArticleList] = useLocalStorage("articles", []);
+  const params = useParams();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // const editArticle = (id, title, content) => {
+  //   setArticleList([
+  //     ...articleList,
+  //     {
+  //       id: id,
+  //       title: title,
+  //       content: content,
+  //     },
+  //   ]);
+  // };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const newState = articleList.map((obj) => {
+      if (obj.id === params.articleId) {
+        return { ...obj, title: title, content: content };
+      }
+
+      return obj;
+    });
+
+    setArticleList(newState);
+    console.log(articleList);
+  };
+
+  const titleOnChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const contentOnChangeHandler = (e) => {
+    setContent(e.target.value);
+  };
+
+  // useEffect(() => {
+  //   const items = localStorage.getItem("articles");
+  //   console.log(articleList);
+  //   // console.log(content);
+  // }, []);
+
+  return (
+    <EditArticleScreenMain>
+      <EditArticleForm
+        title={title}
+        content={content}
+        handleUpdate={handleUpdate}
+        titleOnChangeHandler={titleOnChangeHandler}
+        contentOnChangeHandler={contentOnChangeHandler}
+      />
+    </EditArticleScreenMain>
+  );
 };
 
 export default EditArticleScreen;
