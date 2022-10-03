c88cb421816c559fff47e4febc65e63f1e72d885
KeithRussel <keithmaniacop@gmail.com>
2022-10-03 18:19:15 +0800

    Refactor Theme props and custom styled component

--- src/Screens/ThemeScreen.js
+++ src/Screens/ThemeScreen.js
@@ -2,6 +2,17 @@ import React, { useEffect } from "react";
 import { useLocalStorage } from "../Components/LocalStorage/localStorage";
 import Theme from "../Components/Theme/Theme";
 import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const Palletes = styled.div`
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin: 1rem 0;
+`;
 
 const ThemeScreen = () => {
   const [colorBackground, setColorBackground] = useLocalStorage("theme", "");
@@ -28,12 +39,26 @@ const ThemeScreen = () => {
   return (
     <div>
       <Link to="/">Back</Link>
-      <Theme
-        colorBackground={colorBackground}
-        blueskyChangeThemeHandler={blueskyChangeThemeHandler}
-        woodChangeThemeHandler={woodChangeThemeHandler}
-        pinkyChangeThemeHandler={pinkyChangeThemeHandler}
-      />
+      <Palletes>
+        <Theme
+          onClick={blueskyChangeThemeHandler}
+          colorName="Sky Blue"
+          classColor="blue"
+          colorBackground={colorBackground}
+        />
+        <Theme
+          onClick={woodChangeThemeHandler}
+          colorName="Wood"
+          classColor="wood"
+          colorBackground={colorBackground}
+        />
+        <Theme
+          onClick={pinkyChangeThemeHandler}
+          colorName="Pinky"
+          classColor="pinky"
+          colorBackground={colorBackground}
+        />
+      </Palletes>
     </div>
   );
 };
