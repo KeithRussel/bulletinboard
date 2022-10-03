f670839d5b1c86caf3dff01f2d87c5201b75a2d1
KeithRussel <keithmaniacop@gmail.com>
2022-10-03 18:18:20 +0800

    Refactor theme component

--- src/Components/Theme/Theme.js
+++ src/Components/Theme/Theme.js
@@ -1,14 +1,56 @@
-const Theme = ({
-  blueskyChangeThemeHandler,
-  woodChangeThemeHandler,
-  pinkyChangeThemeHandler,
-}) => {
+import styled from "@emotion/styled";
+
+const Pallete = styled.div`
+  border: 2px solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 15px;
+  background-color: #ececec;
+
+  .blue {
+    background-color: #a3e3fd;
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+    border: 1px solid;
+  }
+
+  .wood {
+    background-color: #eecda3;
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+    border: 1px solid;
+  }
+
+  .pinky {
+    background-color: #fdd1fc;
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+    border: 1px solid;
+  }
+
+  button {
+    margin: 1rem 0;
+    width: 150px;
+    height: 40px;
+    background-color: #29203d;
+    color: white;
+    border: 1px solid #ccc;
+  }
+`;
+
+const Theme = ({ onClick, colorName, classColor }) => {
   return (
-    <div>
-      <button onClick={blueskyChangeThemeHandler}>Blue Sky</button>
-      <button onClick={woodChangeThemeHandler}>Wood</button>
-      <button onClick={pinkyChangeThemeHandler}>Pinky</button>
-    </div>
+    <Pallete>
+      <h3>{colorName}</h3>
+      <div className={classColor}></div>
+      <button type="button" onClick={onClick}>
+        Set as theme
+      </button>
+    </Pallete>
   );
 };
 
