a742b46e98042947b3d42b92e930a70fad7ed7f4
KeithRussel <keithmaniacop@gmail.com>
2022-10-03 16:46:18 +0800

    Change Link style from fixed to absolute

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -3,13 +3,20 @@ import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import Articles from "../Components/Articles/Articles";
 import { useLocalStorage } from "../Components/LocalStorage/localStorage";
-import { MdOutlineAddCircle } from "react-icons/md";
+import { MdOutlineAddCircle, MdSettings } from "react-icons/md";
 
 const AddArticleRoute = styled.span`
-  position: fixed;
+  position: absolute;
   left: auto;
   right: 25px;
   bottom: 25px;
+`;
+
+const SetTheme = styled.span`
+  position: absolute;
+  left: auto;
+  right: 25px;
+  bottom: 100px;
 `;
 
 const HomeScreen = () => {
@@ -41,6 +48,11 @@ const HomeScreen = () => {
           <MdOutlineAddCircle size={50} color={"#29203D"} />
         </Link>
       </AddArticleRoute>
+      <SetTheme>
+        <Link to="theme">
+          <MdSettings size={50} color={"#29203D"} />
+        </Link>
+      </SetTheme>
     </div>
   );
 };
